842b7c55abc4c6c3fc9168bcdbf23c4e932a81c5
Khalil <Khalil.Zeiaei@Gmail.com>
2021-05-12 19:08:44 +0430

    add Remove from basket

--- lib/ShoppingBasket.dart
+++ lib/ShoppingBasket.dart
@@ -3,7 +3,6 @@ import 'package:people/ShoppingBasketItem.dart';
 import 'ShoppingBasketData.dart';
 import 'MyConfig.dart';
 
-
 class ShoppingBasket extends StatefulWidget {
   @override
   _ShoppingBasketState createState() => _ShoppingBasketState();
@@ -13,53 +12,76 @@ class _ShoppingBasketState extends State<ShoppingBasket> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:
-        Text(
-          App_Cap,
-          style: TextStyle(fontFamily: my_txt_font, color: my_txtTitle_color),
+        appBar: AppBar(
+          title: Text(
+            "سبد خرید",
+            style: TextStyle(fontFamily: my_txt_font, color: my_txtTitle_color),
+          ),
+          backgroundColor: Colors.black12,
+          elevation: 1,
+          centerTitle: true,
+
+          leading: Icon(Icons.arrow_back_ios),
+          // actions: <Widget>[ IconButton(icon: Icons.map, color: Colors.black38,)],
         ),
-        backgroundColor: Colors.black12,
-        elevation: 1,
-        centerTitle: true,
-
-        leading: Icon(Icons.arrow_back_ios),
-        // actions: <Widget>[ IconButton(icon: Icons.map, color: Colors.black38,)],
-      ),
-      body: BasketUI()
-
-    );
+        body: BasketUI());
   }
 
   BasketUI() {
-    return ListView.builder(
-      itemCount: ShoppingBasketData.getInstance().basketItems.length,
-        itemBuilder: (context, position){
-          return GestureDetector(
-            child: Padding(
-                child: ShoppingBasketItem(ShoppingBasketData.getInstance().basketItems[position], removeItem , position),
-              padding: EdgeInsets.only(left:10, right: 10, top:10),
+    return Stack(
+      children: [
+        ListView.builder(
+            itemCount: ShoppingBasketData.getInstance().basketItems.length,
+            itemBuilder: (context, position) {
+              return GestureDetector(
+                child: Padding(
+                  child: ShoppingBasketItem(
+                      ShoppingBasketData.getInstance().basketItems[position],
+                      removeItem,
+                      position),
+                  padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+                ),
+              );
+            }),
+        Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20, left: 10, right: 10),
+              child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Material(
+                    color: Colors.red,
+                    child: InkWell(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
+                        height: 100,
+                        child: Center(
+                          child: Text(
+                            "پرداخت",
+                            style: TextStyle(
+                                fontFamily: "Vazir",
+                                fontSize: 20,
+                                color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )),
             ),
-          );
-        }
+          ],
+        )
+      ],
     );
   }
-  void removeItem(int index)
-  {
+
+  void removeItem(int index) {
     // if(onClicked) {
-      setState(() {
-        if (ShoppingBasketData
-            .getInstance()
-            .basketItems
-            .isNotEmpty) {
-          ShoppingBasketData
-              .getInstance()
-              .basketItems
-              .removeAt(index);
-          print('removeAt($index)');
-        }
-      });
+    setState(() {
+      if (ShoppingBasketData.getInstance().basketItems.isNotEmpty) {
+        ShoppingBasketData.getInstance().basketItems.removeAt(index);
+        print('removeAt($index)');
+      }
+    });
     // }
   }
-
 }
